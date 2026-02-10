99c3b0a8d6d63013e0675c75494235e15b982c06
GringoGrigg <equidza@gmail.com>
2026-02-10 11:20:19 +0300

    arithmetic

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-// 1. Set a variable called someName equal to your name
-let someName = "Your Name";  // Replace "Your Name" with your actual name
-console.log("Name variable created:", someName);
+
+
+let someName = "Greg";
+console.log("Name variable updated again:", someName);
