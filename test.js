1bb93b01a1fe0547a9481f714508bc11a8a1b85a
GringoGrigg <equidza@gmail.com>
2026-02-12 15:25:16 +0300

    age limit for school

--- test.js
+++ test.js
@@ -10,21 +10,21 @@ console.log("Name variable updated again:", someName);
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
 </head>
 <body>
     <h1>JAVASCRIPT COMPARISONS</h1>
     <p>INPUT YOUR AGE AND CLICK THE BUTTON</p>
-    <input id="age" value="18"/>
-   <button onclick="myFunction()">INPUT YOUR AGE</button>
+    <input id="age"  value="18" />
+   <button onclick=" myFunction()" >INPUT YOUR AGE </button>
+
    <p id="demo"></p>
+
     <script>
         function myFunction() { 
             let voteable;
             let age = Number(document.getElementById("age").value); 
-            if (isNan(age)){
+            if (isNaN(age)){
                 voteable= INPUT IS NOT A NUMBER!;
             }
             else{
