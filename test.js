2857bda4bebdf9e2fe5ed75a5a2554cccf1f3b25
GringoGrigg <equidza@gmail.com>
2026-02-12 15:08:42 +0300

    functions on strings

--- test.js
+++ test.js
@@ -2,3 +2,38 @@
 
 let someName = "Greg";
 console.log("Name variable updated again:", someName);
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>JAVASCRIPT COMPARISONS</h1>
+    <p>INPUT YOUR AGE AND CLICK THE BUTTON</p>
+    <input id="age" value="18"/>
+   <button onclick="myFunction()">INPUT YOUR AGE</button>
+   <p id="demo"></p>
+    <script>
+        function myFunction() { 
+            let voteable;
+            let age = Number(document.getElementById("age").value); 
+            if (isNan(age)){
+                voteable= INPUT IS NOT A NUMBER!;
+            }
+            else{
+                voteable = (age <18) ? "STILL YOUNG TO VOTE" : "LEGIBLE TO VOTE";
+            }
+            
+ document.getElementById("demo").innerHTML = voteable + "to vote";
+        }   
+ </script>
+
+</body>
+</html>
